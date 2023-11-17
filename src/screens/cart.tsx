7cd62ff21f3c57bd5465db0ceb23379b24dc2d48
Rai Rossi <109825210+raissarossi@users.noreply.github.com>
2023-11-17 16:38:29 -0300

    gesturehandlerrootview implemented

--- src/screens/cart.tsx
+++ src/screens/cart.tsx
@@ -1,52 +1,55 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Image, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Cart() {
   return (
-    <ScrollView style={styles.container}>
-      <Image
-        source={require("../../assets/images/home/home1.png")}
-        resizeMode="cover"
-        style={styles.image1}
-      />
-      <View style={styles.title1}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+
+      <ScrollView style={styles.container}>
         <Image
-          source={require("../../assets/images/home/title1.png")}
+          source={require("../../assets/images/home/home1.png")}
           resizeMode="cover"
+          style={styles.image1}
         />
-      </View>
-      <View style={styles.comboText}>
-        <Text style={styles.text}>
-          Aqui você encontra produtos para deixar seu ambiente mais limpo e
-          confortável.
-        </Text>
-        <Text style={styles.text}>Alta qualidade e preço justo.</Text>
-        <View>
-          <Text>Product</Text>
-          <Text>Product</Text>
-          <Text>Product</Text>
-          <Text>Product</Text>
-        </View>
         <View style={styles.title1}>
           <Image
-            source={require("../../assets/images/home/title2.png")}
+            source={require("../../assets/images/home/title1.png")}
             resizeMode="cover"
           />
         </View>
-        <View>
-          <Text>Product</Text>
-          <Text>Product</Text>
-          <Text>Product</Text>
-          <Text>Product</Text>
+        <View style={styles.comboText}>
+          <Text style={styles.text}>
+            Aqui você encontra produtos para deixar seu ambiente mais limpo e
+            confortável.
+          </Text>
+          <Text style={styles.text}>Alta qualidade e preço justo.</Text>
+          <View>
+            <Text>Product</Text>
+            <Text>Product</Text>
+            <Text>Product</Text>
+            <Text>Product</Text>
+          </View>
+          <View style={styles.title1}>
+            <Image
+              source={require("../../assets/images/home/title2.png")}
+              resizeMode="cover"
+            />
+          </View>
+          <View>
+            <Text>Product</Text>
+            <Text>Product</Text>
+            <Text>Product</Text>
+            <Text>Product</Text>
+          </View>
         </View>
-      </View>
-      <Image
-        source={require("../../assets/images/home/home2.png")}
-        resizeMode="cover"
-        style={styles.image1}
-      />
-    </ScrollView>
+        <Image
+          source={require("../../assets/images/home/home2.png")}
+          resizeMode="cover"
+          style={styles.image1}
+        />
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 }
 
